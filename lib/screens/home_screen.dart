1700d79a4ff8fff66571beeae66d486909a0096a
Mohamed Alsaee <44772147+mohamed97alsaee@users.noreply.github.com>
2023-06-21 18:53:42 +0200

    21 jun 7:00 pm

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,8 @@
-
-
-
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
 
   final String title;
 
@@ -16,90 +10,187 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
-
-
-
-
-
-
 class _MyHomePageState extends State<MyHomePage> {
+  bool selectedStatus = false;
+  TextEditingController titleController = TextEditingController();
+  TextEditingController subTitleController = TextEditingController();
 
+  List tasks = [];
 
-List tasks = [
-  {'title' : 'Todo App',
-   'subtitle': "Hello this is subtitle !",
-   'isDone' : false},
-  {'title' : 'Todo App', 'subtitle': "Hello this is subtitle !" ,'isDone' : false},
-  {'title' : 'Todo App', 'subtitle': "Hello this is subtitle !" ,'isDone' : false},
-  {'title' : 'Todo App', 'subtitle': "Hello this is subtitle !" ,'isDone' : false},
-  {'title' : 'Todo App', 'subtitle': "Hello this is subtitle !" ,'isDone' : false},
-  {'title' : 'Todo App', 'subtitle': "Hello this is subtitle !" ,'isDone' : false},
-  {'title' : 'Todo App', 'subtitle': "Hello this is subtitle !" ,'isDone' : false},
-
-
-];
-
-@override
+  @override
   void initState() {
-if (kDebugMode) {
-  print('HELLO FROM HOME INIT')   ;
-} super.initState();
+    if (kDebugMode) {
+      print('HELLO FROM HOME INIT');
+    }
+    super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
-      body:
-  DefaultTabController(
-    length: 2,
-    child: Column(
-      children: [
-        const TabBar(
-        labelColor: Colors.black,
-        tabs:  [
-          Tab(
-            text: 'Waiting',
-            
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  child: Text(
+                    'Done',
+                    style: TextStyle(
+                        color: selectedStatus == true
+                            ? Colors.green
+                            : Colors.black87),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      selectedStatus = true;
+                    });
+                  },
+                ),
+                GestureDetector(
+                  child: Text(
+                    'Waiting',
+                    style: TextStyle(
+                        color: selectedStatus == false
+                            ? Colors.green
+                            : Colors.black87),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      selectedStatus = false;
+                    });
+                  },
+                ),
+              ],
+            ),
           ),
-           Tab(
-            text: 'Done',
-            
-          ),
-        ]
-        
-      
-        ),
-  
-  Expanded(child: TabBarView(children:  [       ListView.builder(
-        itemCount:  tasks.length,
-        shrinkWrap: true,
-  
-         itemBuilder: (context , index){
-        return ListTile(
-        title: Text(tasks[index]['title'] , ),
-        subtitle:  Text(tasks[index]['subtitle'] , style:  TextStyle( color: Colors.black38 , decoration: tasks[index]['isDone']  ? TextDecoration.lineThrough : TextDecoration.none ), ),
-        trailing: Checkbox(value: tasks[index]['isDone'] ,
-        onChanged: (value){
-   setState(() {
-        tasks[index]['isDone'] = !tasks[index]['isDone']  ;
-        });
-        }, ),);
-  
-         },
-         ),
-     const Center(child: Text('DONE'),)] ))
-    ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasks.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return selectedStatus == tasks[index]['isDone']
+                    ? ListTile(
+                        title: Text(
+                          tasks[index]['title'],
+                          style: TextStyle(
+                              decoration: tasks[index]['isDone']
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tasks[index]['subtitle'].toString(),
+                              style: TextStyle(
+                                  decoration: tasks[index]['isDone']
+                                      ? TextDecoration.lineThrough
+                                      : TextDecoration.none),
+                            ),
+                            Text(
+                              tasks[index]['created_at']
+                                  .toString()
+                                  .substring(0, 10)
+                                  .replaceAll('-', '/'),
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 13,
+                                  decoration: tasks[index]['isDone']
+                                      ? TextDecoration.lineThrough
+                                      : TextDecoration.none),
+                            ),
+                          ],
+                        ),
+                        trailing: Checkbox(
+                          value: tasks[index]['isDone'],
+                          onChanged: (value) {
+                            setState(() {
+                              tasks[index]['isDone'] = !tasks[index]['isDone'];
+                            });
+                          },
+                        ),
+                      )
+                    : Container();
+              },
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return Dialog(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Form(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text('Hello From Dialog'),
+                          TextFormField(
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            controller: titleController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              if (value.length <= 3) {
+                                return 'Please enter valid task';
+                              }
+                            },
+                          ),
+                          TextFormField(
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            controller: subTitleController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              if (value.length <= 3) {
+                                return 'Please enter valid task';
+                              }
+                            },
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                if (titleController.text.isNotEmpty &&
+                                    subTitleController.text.isNotEmpty) {
+                                  setState(() {
+                                    tasks.add(
+                                      {
+                                        'title': titleController.text,
+                                        'subtitle': subTitleController.text,
+                                        'isDone': false,
+                                        "created_at": DateTime.now()
+                                      },
+                                    );
+                                  });
+                                  titleController.clear();
+                                  subTitleController.clear();
 
-
-    ),
-
-  
-  )
+                                  Navigator.pop(context);
+                                }
+                              },
+                              child: const Text('Add')),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              });
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
